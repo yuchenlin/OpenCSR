5c2585f4564d60fca90cba16c2d9b89ac3133f72
(Bill) Yuchen Lin <yuchen.lin@usc.edu>
2021-06-05 22:01:00 -0700

    clean the code

--- baseline_methods/BM25/search.py
+++ baseline_methods/BM25/search.py
@@ -32,29 +32,21 @@ def get_top_k(query, k):
   return facts, scores
 
 
-def save_BM25_hits():
+def save_BM25_hits(threshold=1000):
   """Save the retrieved results."""
   with open(FLAGS.linked_qa_file) as f:
     instances = [json.loads(line) for line in f.read().split("\n") if line]
 
-  num_questions = len(instances)
-  thresholds = [
-      10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700,
-      800, 900, 1000]
-  # num_first_position = [0] * len(thresholds)
-  # num_last_position = [0] * len(thresholds)
+  num_questions = len(instances)  
   result_data = []
-  # num_top_correct = 0
   for ins in tqdm(instances[:], desc=FLAGS.linked_qa_file):
     q = ins["question"]
     q = urllib.parse.quote_plus(q) 
-    ret_facts, ret_scores = get_top_k(q, k=thresholds[-1])
+    ret_facts, ret_scores = get_top_k(q, k=threshold)
     all_ret_facts = [(f["id"], s) for f, s in zip(ret_facts, ret_scores)]
-    
     BM25_res = {}    
     BM25_res["all_ret_facts"] = all_ret_facts
     ins["results"] = BM25_res
-
     result_data.append(ins)
 
   with open(FLAGS.linked_qa_file.replace(".jsonl", "")+".BM25.jsonl", "w") as f:
